51012efdf0a21316cda63a3e7e372f578a292c9f
Vishal Dhole <61483500+vishaldhole173@users.noreply.github.com>
2022-01-11 10:30:41 +0530

    Update createtable.js

--- createtable.js
+++ createtable.js
@@ -6,7 +6,7 @@ const options = {
       host : '127.0.0.1',
       port : 3306,
       user : 'root',
-      password : '9850500930',
+      password : '****',
       database : 'userdata'
     }
 }
